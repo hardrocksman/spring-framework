4dfb19140911dcdbbfe8b58862b8cf0e5a929de1
hardrock <zhenghl0517@163.com>
2020-08-05 23:33:06 +0800

    add config demo

--- spring-hardrock/src/main/java/com/spring/test/hardrock/Test.java
+++ spring-hardrock/src/main/java/com/spring/test/hardrock/Test.java
@@ -1,20 +1,28 @@
 package com.spring.test.hardrock;
 
+import com.spring.test.hardrock.config.TestConfig;
 import com.spring.test.hardrock.service.HelloService;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("test");
+//		System.out.println("test");
+//
+//		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("application.xml");
+//		//2.通过IOC容器对象来得到Helloworld对应的对象，利用bean id来唯一标识这个对象
+//		HelloService helloWorld = applicationContext.getBean(HelloService.class);
+//		//3.调用sayHello()方法
+//		String result = helloWorld.sayHello("hard");
+//
+//		System.out.println("result:" + result);
 
-		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("application.xml");
-		//2.通过IOC容器对象来得到Helloworld对应的对象，利用bean id来唯一标识这个对象
-		HelloService helloWorld = applicationContext.getBean(HelloService.class);
-		//3.调用sayHello()方法
-		String result = helloWorld.sayHello("hard");
-
+		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+		HelloService helloService = ac.getBean(HelloService.class);
+		String result = helloService.sayHello("hard");
 		System.out.println("result:" + result);
+
 	}
 }
---
+++ spring-hardrock/src/main/java/com/spring/test/hardrock/config/TestConfig.java
@@ -0,0 +1,9 @@
+package com.spring.test.hardrock.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.ComponentScan;
+
+@Configurable
+@ComponentScan("com.spring.test.hardrock")
+public class TestConfig {
+}
